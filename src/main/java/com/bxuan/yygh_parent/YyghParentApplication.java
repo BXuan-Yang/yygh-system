9ca7683d983aff638bee64354e2b179869a1a31d
BXuan <ybxuan2002@163.com>
2022-09-20 15:00:17 +0800

    第一版

---
+++ src/main/java/com/bxuan/yygh_parent/YyghParentApplication.java
@@ -0,0 +1,13 @@
+package com.bxuan.yygh_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyghParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyghParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bxuan/yygh_parent/YyghParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bxuan.yygh_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
